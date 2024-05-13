f1c6558982ea8b2df54c43523a9aea88e59b1b51
Zion <mrzz@tuta.io>
2024-05-13 16:53:49 +0200

    upgrade to cancun version with solc 0.8.24

--- contracts/OrderAdapter.sol
+++ contracts/OrderAdapter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OFTAdapter } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTAdapter.sol";
