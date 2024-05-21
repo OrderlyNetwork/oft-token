f1582e9f031f03efedd6b580076f8990d31ce88e
Zion <mrzz@tuta.io>
2024-05-21 13:21:28 +0200

    disable zero token action for bridge

--- contracts/OrderAdapter.sol
+++ contracts/OrderAdapter.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OFTAdapter } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTAdapter.sol";
 
@@ -10,4 +11,26 @@ contract OrderAdapter is OFTAdapter {
         address _lzEndpoint,
         address _delegate
     ) OFTAdapter(_orderToken, _lzEndpoint, _delegate) Ownable(_delegate) {}
+
+    // disable zero token transfer
+    function _debit(
+        address _from,
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        uint32 _dstEid
+    ) internal virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        if (_amountLD > 0) {
+            (amountSentLD, amountReceivedLD) = super._debit(_from, _amountLD, _minAmountLD, _dstEid);
+        }
+    }
+
+    function _credit(
+        address _to,
+        uint256 _amountLD,
+        uint32 _srcEid
+    ) internal virtual override returns (uint256 amountReceivedLD) {
+        if (_amountLD > 0) {
+            amountReceivedLD = super._credit(_to, _amountLD, _srcEid);
+        }
+    }
 }
--- contracts/OrderOFT.sol
+++ contracts/OrderOFT.sol
@@ -3,10 +3,34 @@ pragma solidity ^0.8.20;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import { Origin } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTCore.sol";
 
 contract OrderOFT is OFT {
+    mapping(uint32 => mapping(bytes32 => uint64)) public receivedNonce;
     constructor(
         address _lzEndpoint,
         address _delegate
     ) OFT("Orderly Network", "ORDER", _lzEndpoint, _delegate) Ownable(_delegate) {}
+
+    // disable zero token transfer
+    function _debit(
+        address _from,
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        uint32 _dstEid
+    ) internal virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        if (_amountLD > 0) {
+            (amountSentLD, amountReceivedLD) = super._debit(_from, _amountLD, _minAmountLD, _dstEid);
+        }
+    }
+
+    function _credit(
+        address _to,
+        uint256 _amountLD,
+        uint32 _srcEid
+    ) internal virtual override returns (uint256 amountReceivedLD) {
+        if (_amountLD > 0) {
+            amountReceivedLD = super._credit(_to, _amountLD, _srcEid);
+        }
+    }
 }
